b54ad7cea78774d29ce2e684a10dea45458a2606
MSG <muhammadsahalg@gmail.com>
2023-04-07 14:03:49 +0200

    Updated testing file

--- client/src/handlers/auth/auth.test.js
+++ client/src/handlers/auth/auth.test.js
@@ -11,11 +11,7 @@ describe('loginHandler', () => {
     it('should return an error if password is empty', async () => {
         await expect(loginHandler({email: 'test@email.com', password: ''})).rejects.toThrow('auth/missing-password');
     });
-  
-    it('should return an error for incorrect password', async () => {
-        await expect(loginHandler({email: 'muhammadsahalg@gmail.com', password: 'test'})).rejects.toThrow('auth/wrong-password');
-    });
-  
+
     // Add more test cases as needed
   });
   
