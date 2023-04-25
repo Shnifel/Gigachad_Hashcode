64cd00ee1e767e2760f978f913f4580512d8e869
MSG <muhammadsahalg@gmail.com>
2023-04-25 10:12:49 +0200

    Fixed issue with register unit test

--- client/src/handlers/auth/auth.test.js
+++ client/src/handlers/auth/auth.test.js
@@ -1,6 +1,4 @@
 
-
-import { describe } from 'node:test';
 import { loginHandler, registerHandler } from './auth';
 
 
@@ -17,9 +15,9 @@ describe('loginHandler', () => {
   });
 
 describe('registerHandler', () => {
-    it('should return an error if email is empty'), async () => {
-        await expect(registerHandler({email: '', password: 'password'})).rejects.toThrow('auth/invalid-email');
-    }
+    it('should return an error if email is empty', async () => {
+        await expect(registerHandler({email: '', password: 'password'})).rejects.toThrow('auth/missing-email');
+    });
 
     it('should return an error if password is empty', async () => {
         await expect(loginHandler({email: 'test@email.com', password: ''})).rejects.toThrow('auth/missing-password');
