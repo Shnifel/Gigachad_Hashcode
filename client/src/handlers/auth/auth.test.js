9c24b0fc419be419ae1fa1decb6461d1772e14fa
MSG <muhammadsahalg@gmail.com>
2023-04-25 10:08:00 +0200

    Added unit tests for registering

--- client/src/handlers/auth/auth.test.js
+++ client/src/handlers/auth/auth.test.js
@@ -1,6 +1,7 @@
 
 
-import { loginHandler } from './auth';
+import { describe } from 'node:test';
+import { loginHandler, registerHandler } from './auth';
 
 
 describe('loginHandler', () => {
@@ -14,5 +15,15 @@ describe('loginHandler', () => {
 
     // Add more test cases as needed
   });
+
+describe('registerHandler', () => {
+    it('should return an error if email is empty'), async () => {
+        await expect(registerHandler({email: '', password: 'password'})).rejects.toThrow('auth/invalid-email');
+    }
+
+    it('should return an error if password is empty', async () => {
+        await expect(loginHandler({email: 'test@email.com', password: ''})).rejects.toThrow('auth/missing-password');
+    });
+})
   
 
