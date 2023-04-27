90369f543c280d63e7aad804a50f94c66b6ff660
Muhammad Sahal Goolam <muhammadsahalg@gmail.com>
2023-04-27 14:13:35 +0200

    Added unit tests for server side competitions CRUD

--- server/handlers/competitions/competitions.js
+++ server/handlers/competitions/competitions.js
@@ -70,24 +70,18 @@ export const getCompetitions = async (req, res) => {
 
 export const getCompetition = async (req, res) => {
     try {
-        const compid = req.body.compid;
-    console.log(compid);
-    const compRef = db.collection("Competitions").doc(compid);
+     const compid = req.body.compid;
+     const compRef = db.collection("Competitions").doc(compid);
 
-    await compRef.get().then(
-        query => {
-            return res.status(200).json(
-                {
-                    id: query.id,
-                    data: query.data()
-                }
-            )
+    const query = await compRef.get();
+    return res.status(200).json(
+        {
+            id: query.id,
+            data: query.data()
         }
-    ).catch(
-        error => {return res.status(400).json(error.message)}
     )
     } catch (error) {
-      return res.status(400).json("Unexpected error")  
+      return res.status(400).json(error.message);  
     }
     
 }
@@ -102,7 +96,8 @@ export const getCompetition = async (req, res) => {
  */
 export const updateCompetition = async(req, res) => {
     //Given competition data, will update any changes made to it
-
+    try {
+        
     const compid = req.body.compid //Extract competition id from request body
     const docRef = db.collection("Competitions").doc(compid);
 
@@ -120,11 +115,16 @@ export const updateCompetition = async(req, res) => {
 
 
     //Perform update
-    docRef.update(updatedData).then(() => {
-        return res.status(200).json("Successfully updated competition data")
-    }).catch((error) => {
-        return res.status(400).json("An error has occurred in attempting to update the competition")
-    })
+    await docRef.update(updatedData)
+    return res.status(200).json("Successfully updated competition data");
+   
+        
+
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+
+    
 }
 
 /**
--- server/handlers/competitions/getCompetitions.test.js
+++ server/handlers/competitions/getCompetitions.test.js
@@ -1,4 +1,4 @@
-import { getCompetitions } from "./competitions";
+import { getCompetitions, getCompetition, updateCompetition } from "./competitions";
 import { db } from '../../database/firebase.js';
 
 const mockQuerySnapshot = {
@@ -41,7 +41,11 @@ const mockQuerySnapshot = {
     return {
       db: {
         collection: jest.fn().mockReturnValue({
-          get: jest.fn()
+          get: jest.fn(),
+          doc: jest.fn().mockReturnValue({
+            get: jest.fn(),
+            update: jest.fn()
+          })
         }),
       },
     };
@@ -116,7 +120,128 @@ const mockQuerySnapshot = {
         }
       ]);
     });
-  
-    
-  
   });
+
+  //**Get competitions function testing */
+
+  describe("getCompetition function", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it("should return competitions data with status 200 for valid competition id", async () => {
+      const mockSnapshot = {
+        id: "competition-1",
+        data: () => ({
+          compname: "Competition 1",
+          compdesc: "A competition",
+          numteams: 10,
+          regstartdate: "2022-05-01",
+          regenddate: "2022-05-10",
+          compdate: "2022-06-01",
+          min_teamsize: 2,
+          max_teamsize: 5,
+          admin: "admin-1",
+          teams: []
+        })
+      }
+      const req = {body: {
+        compid: "competition-1"
+      }};
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockReturnThis()
+      };
+      const mockCollectionRef = db.collection('Competitions');
+      const mockCollectionDoc = mockCollectionRef.doc(req.body.compid);
+      mockCollectionDoc.get.mockResolvedValue(mockSnapshot);
+      await getCompetition(req, res);
+      expect(db.collection).toHaveBeenCalledWith("Competitions");
+      expect(db.collection().doc).toHaveBeenCalledWith(req.body.compid);
+      expect(db.collection().doc().get).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({
+        id: "competition-1",
+        data:  {
+          compname: "Competition 1",
+          compdesc: "A competition",
+          numteams: 10,
+          regstartdate: "2022-05-01",
+          regenddate: "2022-05-10",
+          compdate: "2022-06-01",
+          min_teamsize: 2,
+          max_teamsize: 5,
+          admin: "admin-1",
+          teams: []
+        }});
+
+    })
+
+    it("should return an error with status 200 for invalid competition id", async () => {
+      const req = {body: {
+        compid: undefined
+      }};
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockReturnThis()
+      };
+      const errorMessage = "Invalid competition"
+      const mockCollectionRef = db.collection('Competitions');
+      const mockCollectionDoc = mockCollectionRef.doc(req.body.compid);
+      mockCollectionDoc.get.mockRejectedValueOnce(new Error(errorMessage));
+      await getCompetition(req, res);
+      expect(db.collection).toHaveBeenCalledWith("Competitions");
+      expect(db.collection().doc).toHaveBeenCalledWith(req.body.compid);
+      expect(db.collection().doc().get).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith(errorMessage);
+
+    })
+  })
+
+  /** Testing functions update */
+
+  describe("updateCompetitions function", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+    
+    it("should correctly update the competition", async () => {
+      const req = {
+        body: {
+          compid: 'competitionId',
+          compname: 'New Competition Name',
+          compdesc: 'New Competition Description',
+          regstartdate: '2023-05-01',
+          regenddate: '2023-05-10',
+          compdate: '2023-05-15',
+          max_teamsize: 5,
+          numteams: 20,
+          min_teamsize: 1,
+        },
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockReturnThis()
+      };
+      const mockCollectionRef = db.collection('Competitions');
+      const mockCollectionDoc = mockCollectionRef.doc(req.body.compid);
+      mockCollectionDoc.get.mockResolvedValue(mockSnapshot);
+      await getCompetition(req, res);
+      expect(db.collection).toHaveBeenCalledWith("Competitions");
+      expect(db.collection().doc).toHaveBeenCalledWith(req.body.compid);
+      expect(db.collection().doc().update).toHaveBeenCalledWith({
+        compname: 'New Competition Name',
+        compdesc: 'New Competition Description',
+        regstartdate: '2023-05-01',
+        regenddate: '2023-05-10',
+        compdate: '2023-05-15',
+        max_teamsize: 5,
+        numteams: 20,
+        min_teamsize: 1,
+      });
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith("Successfully updated competition data");
+    })
+  })
+
