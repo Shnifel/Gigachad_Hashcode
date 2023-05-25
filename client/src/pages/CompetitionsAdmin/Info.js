aae1aeef6545464b0b631eede1696853618c1589
MSG <muhammadsahalg@gmail.com>
2023-05-25 21:59:20 +0200

    Added editables for comp admin

--- client/src/pages/CompetitionsAdmin/Info.js
+++ client/src/pages/CompetitionsAdmin/Info.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { darkTheme } from '../../components/styles/Theme'
-import { Box, CssBaseline, ThemeProvider, Typography } from '@material-ui/core'
+import { Box, CssBaseline, TextField, ThemeProvider, Typography, Grid } from '@material-ui/core'
 import '../Competitions/competition.scss';
 import '../login.scss';
+import { useState } from 'react';
+import { IconButton } from '@mui/material';
+import { Edit, Save } from '@mui/icons-material';
+import { MarkdownTextbox } from '../../components/MarkdownTextBox.js';
+
 
 const InfoAdmin = (props) => {
    const data = props.data;
@@ -14,23 +19,45 @@ const InfoAdmin = (props) => {
    const min_teamsize = data.min_teamsize;
    const num_tests = data.num_tests;
    const compenddate = data.compenddate;
+
+   const [editMode, setEditMode] = useState(true);
+   
+
+   const toggleEdit = () => {
+    setEditMode(!editMode);
+   }
+
+
    
 
   return (
     <ThemeProvider theme={darkTheme}>
         <CssBaseline/>
         <Box sx={{m: 2}} >
+        <Box sx = {{display: 'flex', width: '100%',justifyContent: 'right'}}>
+        <IconButton onClick={toggleEdit} color='inherit'>
+        {editMode ? <Save /> : <Edit />}
+      </IconButton>
+       </Box>
         <Typography  variant= "h1"  style = {{ fontSize: 50, fontStyle: 'bold', color: "#f500ff", margin: 2 , fontFamily: 'Arcade'}}>
          Description
          </Typography>
             
-            <Typography component="h3" style={{margin: 20}}>
+          {editMode ? <TextField value={description} style={{margin: 20, fontSize: 30}}/>
+          :<Typography component="h2" style={{margin: 20, fontSize: 30}}>
                 {description}
-            </Typography>
-
+            </Typography>}
             <Typography  variant= "h1"  style = {{ fontSize: 50, fontStyle: 'bold', color: "#2A3492", margin: 2 , fontFamily: 'Arcade'}}>
          Dates
          </Typography>
+
+         {editMode ? <Grid item>
+          <Grid container>
+            Registration opens : {regstartdate}
+          </Grid>
+         </Grid> :
+         <Grid item>
+          </Grid>}
 
          <Typography component="h3" style={{margin: 20}} >
                 Competition starts : 
@@ -52,6 +79,8 @@ const InfoAdmin = (props) => {
             <li> Submit both your zip files and outputs as text files for each test case</li>
           </ul>
             </Typography>
+
+            <MarkdownTextbox/>
         </Box>
     </ThemeProvider>
   )
