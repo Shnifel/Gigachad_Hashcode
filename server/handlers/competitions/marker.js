436b4619965b4dc674258cff090f89dec0040e2b
Shnifel <LukeKerkerz@Gmail.com>
2023-05-25 18:42:07 +0200

    Comments

--- server/handlers/competitions/marker.js
+++ server/handlers/competitions/marker.js
@@ -3,10 +3,13 @@ import { spawn } from "child_process";
 
 const downloadFile = async (path) => {
     try {
+        //Doc cloud storage file
         const file = bucket.file(path);
+    //Download File for given path   
     const [data] = await file.download();
     return data;
     } catch (error) {
+        //Throw Error for invalid File Path
         throw new Error(error.message);
     }
     
@@ -15,9 +18,11 @@ const downloadFile = async (path) => {
 export const markFile = (markerPath, testPath, test_case) => {
   return new Promise(async (resolve, reject) => {
     try {
+      //Download Marker File and User Solution
       const markerFile = await downloadFile(markerPath);
       const subFile = await downloadFile(testPath);
-
+      //Run Python File on Solution File and TestCase Number
+      //Will execute as: python marker.py test_case subfilecontents.txt
       let pythonProcess;
       try {
         pythonProcess = spawn('python', ['-c', markerFile.toString(), test_case, subFile.toString()])
@@ -26,24 +31,26 @@ export const markFile = (markerPath, testPath, test_case) => {
       }
 
       let output = '';
+      //Python Program successful, we obtain output
       pythonProcess.stdout.on('data', data => {
         output = data.toString();
       });
-
+      //Python Program failed, throw error
       pythonProcess.stderr.on("error", err => {
         reject(new Error(err));
       });
-
+      //Timeout on Python process to ensure it does not run infinitly
       const timeout = setTimeout(() => {
         pythonProcess.kill();
         reject(new Error('Marking timed out after 1s'));
       }, 1000);
-
+      //Check Terminal Python Process
       pythonProcess.on('close', (code) => {
         clearTimeout(timeout);
         if (code !== 0) {
           reject(new Error('An error has occurred'));
         } else {
+          //Return Score
           resolve(output);
         }
       });
--- server/handlers/competitions/submissions.js
+++ server/handlers/competitions/submissions.js
@@ -5,42 +5,47 @@ import { markFile } from "./marker.js";
 export const addSubmission = async (req, res) => {
 
     try {
+        //Get Submission Document ID
         const subsid = req.body.subsid;
+        //Get CompID
         const compid = req.body.compid;
+        //Get Test Case Number
         const test_case = req.body.test_case;
+        //Retrieve Submission Data for given team
         const subsRef = db.collection("Submissions").doc(subsid);
 
         const data = (await subsRef.get()).data();
         const max_score = data.max_scores[test_case - 1];
-
+        //Call Marker File to mark current submission
         let feedback;
         try {
             feedback = await markFile(compid + "/marker.py", compid + "/submissions/" + subsid + "/test_case_" + test_case + ".txt", test_case);
         } catch (error) {
+            //Marking Time out or invalid format return error
             feedback = error.message;
             const subData = {time: new Date().toLocaleString(), score: -1, feedback, test_case}
             await subsRef.update({subs_history: Admin.firestore.FieldValue.arrayUnion(subData)})
             return res.status(400).json(feedback);
         }
-
-
+        //Marking Succesful, Obtain Score
+        
         const score = parseInt(feedback);
         const subData = {time: new Date().toLocaleString(), score, test_case}
 
-
+        //Add to submissions history 
         await subsRef.update({subs_history: Admin.firestore.FieldValue.arrayUnion(subData)})
-
+        //Keeping Max Score of User's Scores
         if (score > max_score){
             const updated_scores = [...data.max_scores]
             updated_scores[test_case - 1] = score
             await subsRef.update({max_scores: updated_scores})
         }
-
+        //Return Score for Succesful Marking
         return res.status(200).json(feedback);
 
 
     } catch (error) {
-
+        //Return Firebase Error
         return res.status(400).json(error.message);
     }
 }
